c2a14715d78c6bad63edf68c7d1d53f6b7d12671
Communication ACIM <acim.mtp.com@gmail.com>
2025-10-15 14:44:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9905784454-cec16",
+  "appId": "1:1078649579567:web:2d59edd070ad0dd12afeda",
+  "apiKey": "AIzaSyDJaj17VOMTzWvPNWfuzjwU6L7-4AtfLb4",
+  "authDomain": "studio-9905784454-cec16.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1078649579567"
+};
